3088d230f396d212d0bb2b812c72c56ef463c4ba
Wavepool-Liu <57471350+Wavepool-Liu@users.noreply.github.com>
2022-04-01 10:22:47 +0800

    Update U_Uploading.py

--- 1.Software/U_Uploading.py
+++ 1.Software/U_Uploading.py
@@ -4,9 +4,9 @@ import os
 
 
 # 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-auth = oss2.Auth('LTAI4G2NRbhj9MXBv7sqBUp8', 'TGy8O7FHKsDC4NJta4PQRCQu6RvEcj')
+# auth = oss2.Auth('LTAI4G2NRbhj9MXBv7sqBUp8', 'TGy8O7FHKsDC4NJta4PQRCQu6RvEcj')
 # Endpoint以杭州为例，其它Region请按实际情况填写。
-bucket = oss2.Bucket(auth, 'http://oss-cn-beijing.aliyuncs.com', 'imt-agv')
+# bucket = oss2.Bucket(auth, 'http://oss-cn-beijing.aliyuncs.com', 'imt-agv')
 
 
 # # 必须以二进制的方式打开文件，因为需要知道文件包含的字节数。
